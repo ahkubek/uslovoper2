22be2b23194d29c766b2004a7931a8d5945fffe7
ahkubek <120653318+ahkubek@users.noreply.github.com>
2023-01-04 19:59:40 +0300

    zadanie1clientOS

--- Main.java
+++ Main.java
@@ -7,6 +7,12 @@ public class Main {
         } else if(clientOS ==0) {
             System.out.println("скачайте приложение для ANDROID по ссылке...");
         }
-
+        System.out.println("Задание2");
+int clientDeviceYear=2016;
+if(clientDeviceYear<2015){
+    System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+} else if (clientDeviceYear>2015){
+    System.out.println ("установите приложение");
+}
     }
 }
