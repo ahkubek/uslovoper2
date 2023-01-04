c245995aab8c01812aa404a3b499b5831558e123
ahkubek <120653318+ahkubek@users.noreply.github.com>
2023-01-04 19:37:10 +0300

    zadanie1clientOS

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int clientOS =0;
+        if (clientOS==1){
+            System.out.println("скачайте приложение для IOS по ссылке...");
+        } else if(clientOS ==0) {
+            System.out.println("скачайте приложение для ANDROID по ссылке...");
+        }
+
+    }
+}
